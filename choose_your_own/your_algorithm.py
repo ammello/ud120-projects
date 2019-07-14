d98c4d4a00dadeb4693f3ad4fbf6bf59bcba7af0
Anderson Mello <anderson.m.mello@gmail.com>
2019-07-14 19:55:27 -0300

    Aula 5

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -31,14 +31,62 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+# K nearest neighbors
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from time import time
 
+print("K nearest neighbors =======================")
+#print(len(features_test)
+clf = KNeighborsClassifier(n_neighbors=13, p=2, metric='euclidean')
 
+t0 = time()
+clf = clf.fit(features_train, labels_train)
+print("tempo de treinamento:", round(time()-t0, 3), "s")
 
+t0 = time()
+pred = clf.predict(features_test)
+print("tempo de predição:", round(time()-t0, 3), "s")
 
+acc = accuracy_score(labels_test, pred)
+print(acc)
 
+print("AdaBoost =======================")
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier
 
+clf_adaboost = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1), n_estimators=50)
+#clf_adaboost = AdaBoostClassifier(n_estimators=100)
+
+t0 = time()
+clf_adaboost = clf_adaboost.fit(features_train, labels_train)
+print("tempo de treinamento:", round(time()-t0, 3), "s")
+
+t0 = time()
+pred_adaboost = clf_adaboost.predict(features_test)
+print("tempo de predição:", round(time()-t0, 3), "s")
+
+acc = accuracy_score(labels_test, pred_adaboost)
+print(acc)
+
+print("RandomForest =======================")
+from sklearn.ensemble import RandomForestClassifier
+rnd_clf = RandomForestClassifier(n_estimators=100, max_leaf_nodes=4, n_jobs=2, random_state=0)
+
+t0 = time()
+rnd_clf = rnd_clf.fit(features_train, labels_train)
+print("tempo de treinamento:", round(time()-t0, 3), "s")
+
+t0 = time()
+pred_rnd = rnd_clf.predict(features_test)
+print("tempo de predição:", round(time()-t0, 3), "s")
+
+acc = accuracy_score(labels_test, pred_rnd)
+print(acc)
 
 try:
-    prettyPicture(clf, features_test, labels_test)
+    #prettyPicture(clf, features_test, labels_test)
+    prettyPicture(clf_adaboost, features_test, labels_test)
 except NameError:
     pass
+
